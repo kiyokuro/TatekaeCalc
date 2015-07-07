b7b7f65ec71387e4b5d981712ba2f0ed667cd635
kiyokuro <kiyo6412@gmail.com>
2015-07-08 00:37:01 +0900

    整数のみ対応の電卓として一応動作する。小数点は非対応。 画面の1行目のViewTextは実際の入力値、2行目のViewTextはデバッグ用に配列の中身を表示。
    小数点に対応。
    1文字消すボタン、ドットのボタンを追加。ドットは処理を実装。1文字消すは処理が未完成。

---
+++ app/src/main/java/com/example/kuro/tatekaecalc/CalcActivity.java
@@ -0,0 +1,12 @@
+package com.example.kuro.tatekaecalc;
+
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by keinon on 2015/07/04.
+ */
+public class CalcActivity extends ActionBarActivity{
+
+}
